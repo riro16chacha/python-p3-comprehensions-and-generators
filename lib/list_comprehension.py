cab668470ce50a6241afaeb34bf4bd70ccb8f46a
Riro Maclaurine Chacha <riro.chacha@student.moringaschool.com>
2024-01-18 13:50:21 +0300

    completed lab

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python3
 
-def return_evens(num_list):
-    pass
+def return_evens(sequence):
+    evens = [num for num in sequence if num % 2 == 0]
+    return evens
 
-def make_exclamation(sentence_list):
-    pass
+result = return_evens([0, 2, 3, 5, 7, 8, 9])
+print(result)
+
+def make_exclamation(sentences):
+    return [sentence + '!' for sentence in sentences]
+
+result = make_exclamation(["Hello", "I'm doing great", "Python is fun"])
+print(result)
